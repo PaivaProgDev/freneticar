3cf1e118c9a3d5cd52a005d5fc6fdbb699a26a54
PaivaProgDev <guipaiva.ferreira2021@gmail.com>
2025-06-29 19:01:56 -0300

    Alterações no App e estilos globais adicionados

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,32 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./index.css";
+import { Link, Route, Routes } from "react-router-dom";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Home from "./pages/home/Home";
+import Gallery from "./pages/Gallery";
+import WhatsappLogo from "./images/whatsapp-logo.png";
+import About from "./pages/about/About";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <main className="min-h-screen">
+      <Link
+        aria-label="Conversar pelo WhatsApp"
+        to={
+          "https://wa.me/5537998223255?text=Ol%C3%A1%20,%20estou%20vendo%20o%20seu%20projeto."
+        }
+        className="fixed z-100 w-12 bottom-5 right-5"
+      >
+        <img src={WhatsappLogo} alt="Logo do Whatsapp para contato" />
+      </Link>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="gallery" element={<Gallery />} />
+        <Route path="about" element={<About />} />
+      </Routes>
+      <Footer />
+    </main>
   );
 }
 
