b88424e58fb14945359615ba64f2686d0d0667c1
PaivaProgDev <guipaiva.ferreira2021@gmail.com>
2025-06-29 18:59:07 -0300

    Página Galeria adicionada

---
+++ src/pages/Gallery.jsx
@@ -0,0 +1,13 @@
+import Collections from './home/Collections'
+
+const Gallery = () => {
+    return (
+        <div className='px-6 pt-6 overflow-hidden text-center pb-12 bg-[var(--color-bg)]'>
+            <h1 className="font-extrabold text-5xl mb-6">Galeria Completa</h1>
+            <p className='text-[var(--color-4)]'>Explore nossa coleção completa de veículos esportivos premium. Cada modelo é uma obra-prima da engenharia automobilística.</p>
+            <Collections showButtonDetails={true} showFilterButtons={true} previewGallery={true} />
+        </div>
+    )
+}
+
+export default Gallery
