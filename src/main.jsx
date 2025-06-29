e8e0c60fc8fcf7841fb9e2f68462694686602fd7
PaivaProgDev <guipaiva.ferreira2021@gmail.com>
2025-06-29 19:02:38 -0300

    BrowserRouter e Contexto adicionado a main

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { ActionsProvider } from "./context/userActions.jsx";
+import { BrowserRouter } from "react-router-dom";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+createRoot(document.getElementById("root")).render(
+  <ActionsProvider>
+    <BrowserRouter>
+      <StrictMode>
+        <App />
+      </StrictMode>
+      ,
+    </BrowserRouter>
+  </ActionsProvider>
+);
